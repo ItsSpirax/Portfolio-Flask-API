3c862880aa53845b1d6d0d3290220a167fe8bdb3
Adith <60130987+ItsSpirax@users.noreply.github.com>
2024-03-11 22:38:40 +0530

    Test

--- server/main.py
+++ server/main.py
@@ -215,10 +215,8 @@ def submitform():
 
 @app.route("/rr/<path:text>", methods=["GET"])
 def readReceipts(text):
-    embed = DiscordEmbed(title="Read Receipt", color="39d874")
-    embed.add_embed_field(name="Link", value=f"/{text}")
+    embed = DiscordEmbed(title=f"Read - {text}", color="39d874")
     embed.add_embed_field(name="Time", value=f"<t:{int(datetime.now().timestamp())}:T>")
-    
     webhook = DiscordWebhook(url=os.environ["DISCORD_WEBHOOK_READ_RECEIPTS_URL"], username="Website - Adith",
                              avatar_url="https://adith.tech/assets/favicon/favicon-32x32.png")
     webhook.add_embed(embed)
